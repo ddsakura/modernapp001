09a053d7499099feba51ceb924b06e52596ceb16
ddsakura <ddsakura@gmail.com>
2022-05-25 21:55:57 +0800

    more complex widget

--- app/src/main/java/cc/ddsakura/modernapp001/MyAppWidget.kt
+++ app/src/main/java/cc/ddsakura/modernapp001/MyAppWidget.kt
@@ -1,17 +1,35 @@
 package cc.ddsakura.modernapp001
 
+import android.content.Context
+import android.util.Log
+import androidx.compose.material.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
+import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.core.intPreferencesKey
+import androidx.glance.Button
+import androidx.glance.GlanceId
 import androidx.glance.GlanceModifier
+import androidx.glance.action.ActionParameters
+import androidx.glance.action.actionParametersOf
+import androidx.glance.action.actionStartActivity
 import androidx.glance.appwidget.GlanceAppWidget
 import androidx.glance.appwidget.GlanceAppWidgetReceiver
+import androidx.glance.appwidget.action.ActionCallback
+import androidx.glance.appwidget.action.actionRunCallback
+import androidx.glance.appwidget.state.updateAppWidgetState
 import androidx.glance.background
+import androidx.glance.currentState
+import androidx.glance.layout.Alignment
 import androidx.glance.layout.Column
+import androidx.glance.layout.Spacer
+import androidx.glance.layout.fillMaxWidth
 import androidx.glance.layout.padding
 import androidx.glance.state.GlanceStateDefinition
 import androidx.glance.state.PreferencesGlanceStateDefinition
 import androidx.glance.text.Text
+import androidx.glance.text.TextAlign
 import androidx.glance.text.TextStyle
 import androidx.glance.unit.ColorProvider
 
@@ -20,19 +38,72 @@ class MyAppWidget : GlanceAppWidget() {
 
     @Composable
     override fun Content() {
+        val prefs = currentState<Preferences>()
+        val count = prefs[countPreferenceKey] ?: 0
         Column(
             modifier = GlanceModifier
                 .padding(8.dp)
-                .background(Color.Yellow)
+                .background(Color.Yellow),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalAlignment = Alignment.CenterVertically
         ) {
+            Button(
+                text = "Hello World + 1",
+                style = TextStyle(color = ColorProvider(Color.Black)),
+                onClick = actionRunCallback<UpdateCountActionCallback>(
+                    parameters = actionParametersOf(
+                        countParamKey to (count + 1)
+                    )
+                )
+            )
             Text(
-                text = "Hello World",
-            style = TextStyle(color = ColorProvider(Color.Black))
+                text = count.toString(),
+                modifier = GlanceModifier.fillMaxWidth(),
+                style = TextStyle(
+                    textAlign = TextAlign.Center,
+                    color = ColorProvider(MaterialTheme.colors.onSurface),
+                )
+            )
+            Spacer(modifier = GlanceModifier.padding(8.dp))
+            Button(
+                text = "Open Activity",
+                style = TextStyle(color = ColorProvider(Color.Black)),
+                onClick = actionStartActivity<MainActivity>()
             )
         }
+    }
+}
+
+class UpdateCountActionCallback : ActionCallback {
+    override suspend fun onRun(
+        context: Context,
+        glanceId: GlanceId,
+        parameters: ActionParameters
+    ) {
+        Log.d("MyAppWidget", "Item with id $glanceId and params $parameters clicked.")
+        val count = requireNotNull(parameters[countParamKey]) {
+            "Add $countParamKey parameter in the ActionParameters of this action."
+        }
+
+        // Update the count in the preferences data store
+        updateAppWidgetState(
+            context = context,
+            definition = PreferencesGlanceStateDefinition,
+            glanceId = glanceId
+        ) { preferences ->
+            preferences.toMutablePreferences()
+                .apply {
+                    this[countPreferenceKey] = count
+                }
+        }
+
+        MyAppWidget().update(context, glanceId)
     }
 }
 
 class MyWidgetReceiver : GlanceAppWidgetReceiver() {
     override val glanceAppWidget: GlanceAppWidget = MyAppWidget()
 }
+
+private val countPreferenceKey = intPreferencesKey("count-key")
+private val countParamKey = ActionParameters.Key<Int>("count-key")
