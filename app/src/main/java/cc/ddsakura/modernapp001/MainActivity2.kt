99a58bae73617421f92ce77355ce4bec71971c95
ddsakura <ddsakura@gmail.com>
2024-12-02 13:40:52 +0800

    refactor: Update layout and theme for MainActivity2 and WebviewActivity
    This commit updates the layout and theme for `MainActivity2` and `WebviewActivity`:
    
    - Changes the root layout of `MainActivity2` and `WebviewActivity` to `CoordinatorLayout`.
    - Adds an `AppBarLayout` with a `Toolbar` to both activities.
    - Sets the background color of the layouts to white.
    - Updates the theme to remove the action bar and set the status bar color.
    - Adjusts the padding of the content to avoid overlapping with the status bar.
    - Removes unnecessary code related to window insets in `MainActivity2` and `WebviewActivity`.

--- app/src/main/java/cc/ddsakura/modernapp001/MainActivity2.kt
+++ app/src/main/java/cc/ddsakura/modernapp001/MainActivity2.kt
@@ -1,7 +1,6 @@
 package cc.ddsakura.modernapp001
 
 import android.os.Bundle
-import android.view.WindowInsetsController
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
@@ -10,27 +9,11 @@ class MainActivity2 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main2)
-
-        // 設置窗口插入監聽器以調整系統欄位的填充
-        ViewCompat.setOnApplyWindowInsetsListener(
-            findViewById(R.id.root_view2)
-        ) { view, windowInsets ->
-            // 獲取系統欄位（狀態欄、導航欄等）的插入
-            val insets = windowInsets.getInsets(WindowInsetsCompat.Type.systemBars())
-            // 根據插入設置視圖的填充
-            view.setPadding(insets.left, insets.top, insets.right, insets.bottom)
-            // 表示插入已被消耗
-            WindowInsetsCompat.CONSUMED
-        }
-
-        // Hide both the navigation bar and the status bar.
-        // SYSTEM_UI_FLAG_FULLSCREEN is only available on Android 4.1 and higher, but as
-        // a general rule, you should design your app to hide the status bar whenever you
-        // hide the navigation bar.
-        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.R) {
-            val controller = window.insetsController
-            // controller?.hide(WindowInsets.Type.statusBars() or WindowInsets.Type.navigationBars())
-            controller?.systemBarsBehavior = WindowInsetsController.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
+        // 確保內容避開狀態欄區域
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.my_app_bar_layout)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
         }
     }
 }
