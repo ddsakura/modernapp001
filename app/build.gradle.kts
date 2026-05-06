ff1a544dd2c5e4cf6fe931ca3a72e07614e37322
ddsakura <ddsakura@gmail.com>
2026-05-07 07:13:05 +0800

    Update minimum SDK version to 24 and upgrade dependencies for improved compatibility

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,7 +12,7 @@ android {
 
     defaultConfig {
         applicationId = "cc.ddsakura.modernapp001"
-        minSdk = 23
+        minSdk = 24
         targetSdk = 35
         versionCode = 1
         versionName = "1.0"
